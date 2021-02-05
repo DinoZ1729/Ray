c1ebef28113e4e6909602fb950d5f615facf5eff
cyber <krtmhyoyo@gmail.com>
2021-02-05 22:58:16 +0200

    some fixes

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
-![Ray Cover Image](src/ray-cover.png)
+![Ray Cover Image](images/ray-cover.png)
+![Ray Linux Cover Image](images/ray-linux.png)
 
 ## Welcome
 
 Welcome to Ray. A C++ program that can do Raytracing in the console. This is pure bit-beauty.
+
+## Building
+
+```bash
+    mkdir build
+    cd build
+    cmake ..
+    cmake --build .
+```
 
 ## License
 
---
+++ src/functions.h
@@ -0,0 +1,13 @@
+#pragma once
+//declaration
+#include "functions.h"
+
+void transformVector(double vec[3],const double m[16]);
+void invert(double inv[16],const double matrix[16]);
+void cross(double r[3],double a[3],double b[3]);
+double magnitute(double r[3]);
+void normalize(double r[3]);
+void scale(double a[3],double k);
+double dot(double a[3], double b[3]);
+void vector(double a[3],double b[3],double c[3]);
+double clamp(double x,double min,double max);
