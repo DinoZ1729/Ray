4d88d4e5b0a2079216c6f0c40d8e9944a22fda15
alexemanuelol <Alexander.Emanuelsson94@gmail.com>
2021-02-14 11:15:10 +0100

    Removed cross function because unused

--- src/functions.cpp
+++ src/functions.cpp
@@ -136,13 +136,6 @@ void invert(double inv[16],const double matrix[16])
         inv[i] *= det;
 }
 
-void cross(double r[3],double a[3],double b[3])
-{
-	r[0]=a[1]*b[2]-a[2]*b[1];
-	r[1]=a[2]*b[0]-a[0]*b[2];
-	r[2]=a[0]*b[1]-a[1]*b[0];
-}
-
 double magnitute(double r[3])
 {
 	return sqrt(r[0]*r[0]+r[1]*r[1]+r[2]*r[2]);
--- src/functions.h
+++ src/functions.h
@@ -3,7 +3,6 @@
 
 void transformVector(double vec[3],const double m[16]);
 void invert(double inv[16],const double matrix[16]);
-void cross(double r[3],double a[3],double b[3]);
 double magnitute(double r[3]);
 void normalize(double r[3]);
 void scale(double a[3],double k);
